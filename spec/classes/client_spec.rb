5accf441170a1a624d45cfaa4ad7b065c047e423
Chris Tessmer <chris.tessmer@onyxpoint.com>
2016-05-13 22:13:49 -0400

    (SIMP-1054) Update static assets to fix spec tests
    Before this patch, this module would fail its spec tests when
    `STRICT_VARIABLES=yes` is set (as it is in Travis CI tests).
    This was caused by the presence of the unset (and unused) variable
    "spec_title" in the modules' fixture hieradata/hiera.yaml.
    
    This commit fixes the issue by removing "spec_title" from
    `hieradata/hiera.yaml`, adding `on_supported_os` support to more spec
    tests and sanitizing the template logic.
    
    SIMP-1054 #comment Fixed pupmod-simp-nfs
    SIMP-1083 #comment Sanitized template
    SIMP-1083 #comment Bumped module & RPM version to 4.4.3
    SIMP-1083 #close Fixed spec failures when `STRICT_VARIABLES=yes`
    
    Change-Id: Ida20baead97985ee7e67473c8f6949103369c691

--- spec/classes/client_spec.rb
+++ spec/classes/client_spec.rb
@@ -1,27 +1,22 @@
 require 'spec_helper'
 
 describe 'nfs::client' do
-  let(:facts) {{
-    :fqdn => 'spec.test',
-    :grub_version => '0.97',
-    :hardwaremodel => 'x86_64',
-    :interfaces => 'lo',
-    :ipaddress_lo => '127.0.0.1',
-    :operatingsystemmajrelease => '6',
-    :operatingsystem => 'RedHat',
-    :operatingsystemmajrelease => '6',
-    :processorcount => 4,
-    :uid_min => '500'
-  }}
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      let(:facts) { facts }
 
-  it { is_expected.to create_class('nfs::client') }
+      context "on #{os}" do
+        it { is_expected.to create_class('nfs::client') }
 
-  context 'base' do
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_class('nfs') }
-    it { is_expected.to create_iptables__add_tcp_stateful_listen('nfs4_callback_port_testnode.example.domain') }
-    it { is_expected.to create_sysctl__value('fs.nfs.nfs_callback_tcpport') }
-    it { is_expected.to create_file('/etc/modprobe.d/nfs.conf').with_content(/options nfs callback_tcpport=876/) }
-    it { is_expected.to create_exec('modprobe_nfs').that_requires('File[/etc/modprobe.d/nfs.conf]') }
+        context 'base' do
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_class('nfs') }
+          it { is_expected.to create_iptables__add_tcp_stateful_listen('nfs4_callback_port_testnode.example.domain') }
+          it { is_expected.to create_sysctl__value('fs.nfs.nfs_callback_tcpport') }
+          it { is_expected.to create_file('/etc/modprobe.d/nfs.conf').with_content(/options nfs callback_tcpport=876/) }
+          it { is_expected.to create_exec('modprobe_nfs').that_requires('File[/etc/modprobe.d/nfs.conf]') }
+        end
+      end
+    end
   end
 end
