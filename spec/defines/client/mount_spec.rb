8dbe15d8ab398b21b3a726c188b1246be00f8316
Trevor Vaughan <tvaughan@onyxpoint.com>
2018-04-25 16:49:37 -0400

    (SIMP-4692) Allow override server auto-detection (#51)
    - Added `nfs::client::mount::autodetect_remote` to override all
      autodetection of whether or not the remote host was the current NFS
      server.
    - Added `nfs::client::mount::stunnel` to allow users to dictate the
      stunnel connection state for specific mounts.
    
    SIMP-4823 #close

--- spec/defines/client/mount_spec.rb
+++ spec/defines/client/mount_spec.rb
@@ -4,7 +4,7 @@ describe 'nfs::client::mount' do
   shared_examples_for "a fact set" do
     it { is_expected.to compile.with_all_deps }
     it { is_expected.to contain_class('nfs::client') }
-    it { is_expected.to create_nfs__client__mount__connection(title).with_nfs_server('1.2.3.4') }
+    it { is_expected.to create_nfs__client__mount__connection(title).with_nfs_server(params[:nfs_server]) }
   end
 
   context 'supported operating systems' do
@@ -42,49 +42,82 @@ describe 'nfs::client::mount' do
       end
 
       context 'with stunnel' do
+        let(:pre_condition) {
+          <<-EOM
+            class { 'nfs::client': stunnel => true }
+          EOM
+        }
+
         let(:params) {{
           :nfs_server  => '1.2.3.4',
           :remote_path => '/home'
         }}
 
-        let(:pre_condition) {
-          <<-EOM
-            class { 'nfs::client':
-              stunnel => true
-            }
-          EOM
-        }
-
         it_behaves_like "a fact set"
 
-        it {
-          is_expected.to contain_autofs__map__entry(title).with_location("127.0.0.1:#{params[:remote_path]}")
-        }
-
+        it { is_expected.to contain_autofs__map__entry(title).with_location("127.0.0.1:#{params[:remote_path]}") }
         it { is_expected.to contain_exec('refresh_autofs') }
         it { is_expected.to contain_stunnel__instance("nfs4_#{params[:nfs_server]}:2049_client").that_notifies('Exec[refresh_autofs]') }
+
+        context 'with nfsv4' do
+          let(:params) {{
+            :nfs_server  => '1.2.3.4',
+            :remote_path => '/home',
+            :nfs_version => 'nfs4'
+          }}
+
+          it_behaves_like "a fact set"
+          it { is_expected.to contain_nfs__client__stunnel__v4("#{params[:nfs_server]}:2049") }
+        end
+
+        context 'without autofs' do
+          let(:params) {{
+            :nfs_server  => '1.2.3.4',
+            :remote_path => '/home',
+            :autofs  => false
+          }}
+
+          it_behaves_like "a fact set"
+
+          it { is_expected.to contain_mount(title).with_device("127.0.0.1:#{params[:remote_path]}") }
+        end
       end
 
-      context 'with stunnel and without autofs' do
+      context 'with firewall enabled' do
         let(:params) {{
           :nfs_server  => '1.2.3.4',
           :remote_path => '/home',
-          :autofs  => false
+          :nfs_version => 'nfs4'
         }}
 
         let(:pre_condition) {
           <<-EOM
-            class { 'nfs::client':
-              stunnel => true
-            }
+            class { 'nfs::client': firewall => true }
           EOM
         }
 
         it_behaves_like "a fact set"
+        it { is_expected.to contain_class('iptables') }
+        it { is_expected.to contain_iptables__listen__tcp_stateful("nfs_callback_#{params[:nfs_server]}") }
+      end
 
-        it {
-          is_expected.to contain_mount(title).with_device("127.0.0.1:#{params[:remote_path]}")
+      context 'when nfs::client::is_server is true but the remote is not the local system' do
+        let(:pre_condition) {
+          <<-EOM
+            class { 'nfs': is_server => true }
+          EOM
         }
+
+        let(:params){{
+          :nfs_server        => '254.16.1.2',
+          :autodetect_remote => false,
+          :stunnel           => false,
+          :remote_path       => '/home'
+        }}
+
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('nfs::client') }
+        it { is_expected.to contain_autofs__map__entry(title).with_location("#{params[:nfs_server]}:#{params[:remote_path]}") }
       end
     end
   end
---
+++ types/securityflavor.pp
@@ -0,0 +1,7 @@
+type Nfs::SecurityFlavor = Enum[
+  'none',
+  'sys',
+  'krb5',
+  'krb5i',
+  'krb5p'
+]
